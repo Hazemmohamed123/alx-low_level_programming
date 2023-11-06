8efdbe1e6ee65acc7de98d7c960163b17f704c6e
Hazemmohamed123 <hm9702776@gmail.com>
2023-11-06 13:03:29 -0800

    fawsefwwaf

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,16 +1,11 @@
-
 #include <stdio.h>
-#define NAME
-
 /**
- * main - check the code for Holberton School students.
+ * main - prints the name of the file it was compiled from.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-#ifdef NAME
 printf("%s\n", __FILE__);
-#endif
 return (0);
 }
--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,10 +1,10 @@
-
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
+/**
+ * File: main
+ *
+ * Desc: header file
+ */
 int _putchar(char c);
 
-#endif /* MAIN_H */
+#endif
